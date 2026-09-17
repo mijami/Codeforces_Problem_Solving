d9031bfa6479b1486e5bb9a1a6577033222c30fb
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:41:44 +0600

    Add Aleksa and Stack [B] from Codeforces

---
+++ 1878/B - Aleksa and Stack/solution.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define ll long long int
+#define ull unsigned long long int
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+#define mx 300001
+#define newline cout << "
+";
+ 
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n,k;
+        cin >> n;
+        map<int,int>mp;
+ 
+         int y=1;
+        for(int i=0;i<n;i++)
+        {
+            cout  << y << " ";
+            y+=2;
+        }
+        cout << "
+";
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    }
+ 
+ 
+    return 0;
+}
