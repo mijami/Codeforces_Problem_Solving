6597e0481f159766c2f860b765f7872fbd3370a5
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:01:41 +0600

    Add Square Counting [A] from Codeforces

---
+++ 1646/A - Square Counting/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define checkmate return 0;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long int a,b;
+        cin >> a >> b;
+        long long int l=0;
+ 
+        b=b/(a*a);
+ 
+ 
+        cout << b << "
+";
+    }
+ 
+ 
+    checkmate
+}
