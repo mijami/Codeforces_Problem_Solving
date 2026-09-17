daf192be4362566b90321a113810f98e448cf7a3
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:23:13 +0600

    Add Immobile Knight [A] from Codeforces

---
+++ 1739/A - Immobile Knight/solution.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define pb push_back
+#define ll long long int
+#define checkmate return 0;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int a,b;
+        cin >> a >> b;
+        if(a<=3 && a>1 && b<=3 && b>1)
+        {
+            cout << 2 << " 2
+";
+        }
+ 
+        else
+        {
+            cout << 1 << " 1
+";
+        }
+    }
+ 
+ 
+ 
+ 
+    checkmate
+}
