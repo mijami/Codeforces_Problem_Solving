8f37da9ea2c47aeabd1c6660b3db4a9d91b359ee
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:03:58 +0600

    Add GCD vs LCM [A] from Codeforces

---
+++ 1665/A - GCD vs LCM/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define checkmate return 0;
+#define pb push_back
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        long long int n;
+        cin >> n;
+        long long int a,b,c,d;
+ 
+        cout << n-3 << " 1 1 1" << "
+";
+    }
+    checkmate
+}
