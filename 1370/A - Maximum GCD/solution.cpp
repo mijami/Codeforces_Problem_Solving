06f70420595772ed64bcefc18b279ab0aeab81e9
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:04:12 +0600

    Add Maximum GCD [A] from Codeforces

---
+++ 1370/A - Maximum GCD/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define checkmate return 0;
+#define pb push_back
+#define ll long long int
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int a;
+        cin >> a;
+ 
+        cout << a/2 << "
+";
+ 
+    }
+ 
+    checkmate
+}
+ 
