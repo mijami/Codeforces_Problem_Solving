5de81abb07a24426307827df224a09495020c65d
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:34:30 +0600

    Add Blackboard List [A] from Codeforces

---
+++ 1838/A - Blackboard List/solution.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define ll long long int
+using namespace std;
+ 
+/*
+vector<pair<int,int>> vec[1000005];
+vector<int> dp;
+map<pair<int,int>,int> rec;
+ 
+ 
+void DFS(int current, int parent)
+{
+    for(auto i:vec[current])
+    {
+        int to= i.first;
+        int id= i.second;
+        if(to==parent)
+        {
+            continue;
+        }
+        rec[{current,to}]=id;
+        rec[{to,current}]=id;
+        if(dp[current]!=INT_MAX)
+        {
+            if(current==1)
+            {
+                dp[to]=1;
+            }
+            else
+            {
+                if(id>rec[{current,parent}])
+                {
+                    dp[to]=dp[current];
+                }
+                else{
+                    dp[to]=dp[current]+1;
+                }
+            }
+        }
+        DFS(to,current);
+    }
+}
+ 
+*/
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        vector<ll>vec;
+        int n;
+        cin >> n;
+ 
+        ll ans;
+        int flag=0;
+        for(int i=0; i<n; i++)
+        {
+            ll a;
+            cin >> a ;
+            vec.pb(a);
+            if(a<0)
+            {
+                ans=a;
+                flag++;
+            }
+        }
+        if(flag)
+        {
+            cout << ans << "
+";
+            continue;
+        }
+        sort(vec.rbegin(),vec.rend());
+ 
+ 
+        cout << vec[0] << "
+";
+ 
+ 
+    }
+ 
+    return 0;
+}
