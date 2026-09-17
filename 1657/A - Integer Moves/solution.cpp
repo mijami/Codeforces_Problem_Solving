ca91556739fa826e02aad77c1b2565b6b6ecdaec
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:04:25 +0600

    Add Integer Moves [A] from Codeforces

---
+++ 1657/A - Integer Moves/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define checkmate return 0;
+#define pb push_back
+#define ll long long int
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int a,b;
+        cin >> a >> b;
+        int k=sqrt(a*a+b*b);
+        if(a==0 && b==0)
+        {
+            cout << 0 << "
+";
+        }
+        else if(sqrt(a*a+b*b)==k)
+        {
+            cout << 1 << "
+";
+        }
+        else cout << 2 << "
+";
+    }
+ 
+    checkmate
+}
