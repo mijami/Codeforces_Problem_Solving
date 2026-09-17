4c60f769ea0ee9b7084b042edf1e4362017107fd
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:43:19 +0600

    Add Game with Integers [A] from Codeforces

---
+++ 1899/A - Game with Integers/solution.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define ll long long int
+#define ull unsigned long long int
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+ 
+#define newline cout << "
+";
+ 
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+ 
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        if(n%3==0)
+        {
+            cout << "Second
+";
+        }
+        else
+        {
+            cout << "First
+";
+        }
+ 
+ 
+ 
+ 
+ 
+    }
+ 
+ 
+    return 0;
+}
