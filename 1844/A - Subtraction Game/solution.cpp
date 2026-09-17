21c001f2fb3d3c30b7e96d0c13a8165d674e2eb2
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:37:13 +0600

    Add Subtraction Game [A] from Codeforces

---
+++ 1844/A - Subtraction Game/solution.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define ll long long int
+using namespace std;
+ 
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t;
+    cin >> t;
+    while(t--)
+    {
+ 
+        int n,k;
+        cin >> n >> k;
+ 
+ 
+        cout << n+k << "
+";
+ 
+ 
+ 
+    }
+ 
+ 
+ 
+ 
+ 
+    return 0;
+}
+ 
