47f03b5910d61ce6ba3d9cd13de5ffc3171ef512
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:06:02 +0600

    Add Complicated GCD [A] from Codeforces

---
+++ 664/A - Complicated GCD/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define checkmate return 0;
+#define pb push_back
+#define ll long long int
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    string a,b;
+    cin >> a >> b;
+    if(a==b)
+    {
+        cout << a << "
+";
+    }
+    else
+        cout << 1 << "
+";
+ 
+    checkmate
+}
