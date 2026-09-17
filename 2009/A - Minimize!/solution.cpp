fd6fe26798ce2b8456e780ec022de4b094c4d46a
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:48:35 +0600

    Add Minimize! [A] from Codeforces

---
+++ 2009/A - Minimize!/solution.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define ll long long int
+#define ull unsigned long long int
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+ 
+#define newline cout << "
+";
+ 
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t=1;
+    cin >> t;
+    while(t--)
+    {
+        int n,m;
+        cin >> n >> m;
+ 
+        
+ 
+        cout << m-n << "
+";
+ 
+ 
+ 
+ 
+ 
+ 
+    }
+ 
+ 
+ 
+ 
+ 
+ 
+    return 0;
+ 
+ 
+}
