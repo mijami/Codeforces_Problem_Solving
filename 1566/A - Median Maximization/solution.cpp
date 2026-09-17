0c8a2bc831629be32c0828513c9d56fbb409e002
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:07:55 +0600

    Add Median Maximization [A] from Codeforces

---
+++ 1566/A - Median Maximization/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define checkmate return 0;
+#define pb push_back
+#define for(i,n) for(int i=0;i<n;i++)
+#define ll long long int
+#define e4;  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+int main()
+{
+    e4;
+ 
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int a,b;
+        cin >> a >> b;
+ 
+        int k=a/2 + 1;
+        int ans=b/k;
+        cout << ans << "
+";
+ 
+ 
+    }
+ 
+    checkmate
+}
