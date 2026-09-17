2732d43cd34b4974c3102cb81b471c8fc234d436
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:25:59 +0600

    Add Is it rated - 2 [A] from Codeforces

---
+++ 1505/A - Is it rated - 2/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define ll long long int
+#define pb push_back
+ 
+ 
+int main()
+{
+ 
+    string a;
+    while(cin >> a)
+    {
+        cout << "NO
+";
+    }
+    return 0;
+}
