625fa1fd45c0687c1594b5dae268570bf5c0c435
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:35:38 +0600

    Add Cipher Shifer [A] from Codeforces

---
+++ 1840/A - Cipher Shifer/solution.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define ll long long int
+using namespace std;
+ 
+/*
+vector<pair<int,int>> vec[1000005];
+vector<int> dp;
+map<pair<int,int>,int> rec;
+ 
+ 
+void DFS(int current, int parent)
+{
+    for(auto i:vec[current])
+    {
+        int to= i.first;
+        int id= i.second;
+        if(to==parent)
+        {
+            continue;
+        }
+        rec[{current,to}]=id;
+        rec[{to,current}]=id;
+        if(dp[current]!=INT_MAX)
+        {
+            if(current==1)
+            {
+                dp[to]=1;
+            }
+            else
+            {
+                if(id>rec[{current,parent}])
+                {
+                    dp[to]=dp[current];
+                }
+                else{
+                    dp[to]=dp[current]+1;
+                }
+            }
+        }
+        DFS(to,current);
+    }
+}
+ 
+*/
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        vector<char>vec;
+        int n;
+        cin >> n;
+        string a;
+        cin >> a;
+        for(int i=0; i<n; i++)
+        {
+            char z=a[i];
+            i++;
+            while(a[i]!=z)
+            {
+                i++;
+            }
+            vec.pb(a[i]);
+        }
+        for(int i=0;i<vec.size();i++)
+        {
+            cout << vec[i];
+        }
+        cout << "
+";
+ 
+    }
+ 
+    return 0;
+}
