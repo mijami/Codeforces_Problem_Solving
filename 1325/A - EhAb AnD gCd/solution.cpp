92d964b59416c3bbf1675c2b6aed00343d36e14f
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:24:04 +0600

    Add EhAb AnD gCd [A] from Codeforces

---
+++ 1325/A - EhAb AnD gCd/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pb push_back
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int a;
+        cin >> a;
+        cout << 1 << " " << a-1 << "
+";
+    }
+ 
+    return 0;
+}
