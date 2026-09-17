51d925464af1e8edbb6cda84fda0ca57ef7d0f30
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:34:02 +0600

    Add Divisible Array [A] from Codeforces

---
+++ 1828/A - Divisible Array/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define ll long long int
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t;
+    cin >> t;
+    int cas=0;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        for(int i=1; i<=n; i++)
+        {
+            cout << i*2 << " ";
+        }
+        cout << "
+";
+ 
+    }
+    return 0;
+}
+ 
+ 
