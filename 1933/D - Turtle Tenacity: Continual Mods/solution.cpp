03db448acb09b8f7fc7eb4f942fabfb5ad164cff
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:45:42 +0600

    Add Turtle Tenacity: Continual Mods [D] from Codeforces

---
+++ 1933/D - Turtle Tenacity: Continual Mods/solution.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define ll long long int
+#define ull unsigned long long int
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+#define newline cout << "
+";
+ 
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        vector<int>vec;
+        for(int i=0; i<n; i++)
+        {
+            int a;
+            cin >> a;
+            vec.pb(a);
+        }
+        sort(vec.begin(),vec.end());
+        int cnt=0;
+        int fl=0;
+ 
+ 
+        for(int i=1; i<n; i++)
+        {
+            if(vec[0]==vec[1])
+            {
+                fl++;
+                if(vec[i]%vec[0] != 0)
+                {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+        if(cnt && fl)
+        {
+            cout << "YES
+";
+        }
+        else if(fl && cnt==0)
+        {
+            cout << "NO
+";
+        }
+        else
+        {
+            cout << "YES
+";
+        }
+ 
+ 
+ 
+ 
+ 
+ 
+    }
+ 
+ 
+    return 0;
+}
