c9ad0328df0dd544fc6a2dfbd62770e9e024f107
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:29:28 +0600

    Add Plus or Minus [A] from Codeforces

---
+++ 1807/A - Plus or Minus/solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define ll long long int
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t;
+    cin >> t;
+    while(t--)
+    {
+ 
+        int a,b,c;
+        cin >> a >> b >> c;
+        if(a+b==c)
+        {
+            cout << "+
+";
+        }
+        else cout << "-
+";
+    }
+ 
+    return 0;
+}
