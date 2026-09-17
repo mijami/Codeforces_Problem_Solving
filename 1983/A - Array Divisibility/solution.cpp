aa805a2fc864609bf7be3443f73c17d2520444d2
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:47:56 +0600

    Add Array Divisibility [A] from Codeforces

---
+++ 1983/A - Array Divisibility/solution.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define ll long long int
+#define ull unsigned long long int
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+ 
+#define newline cout << "
+";
+ 
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t=1;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        for(int i=0;i<n;i++)
+        {
+            cout << i+1 << " ";
+        }
+        cout << "
+";
+ 
+ 
+    }
+ 
+ 
+    return 0;
+}
