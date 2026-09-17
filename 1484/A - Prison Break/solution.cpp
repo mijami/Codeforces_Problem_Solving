12d76c6acf3b621a0d22b555d7a535eba6ba0f45
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:11:53 +0600

    Add Prison Break [A] from Codeforces

---
+++ 1484/A - Prison Break/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define checkmate return 0;
+#define pb push_back
+#define ll long long int
+#define e4  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+int main()
+{
+    e4
+ 
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int a,b;
+        cin >> a >> b;
+        cout << a*b << "
+";
+    }
+ 
+    checkmate
+}
