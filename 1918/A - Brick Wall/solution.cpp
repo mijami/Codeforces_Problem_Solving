1520845b15a1386c46161a77bfbc92882617e2cf
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:44:37 +0600

    Add Brick Wall [A] from Codeforces

---
+++ 1918/A - Brick Wall/solution.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define ll long long int
+#define ull unsigned long long int
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+ 
+#define newline cout << "
+";
+ 
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+ 
+ 
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int a,b;
+        cin >> a >> b;
+        cout << a*(b/2) << "
+";
+ 
+ 
+    }
+ 
+ 
+ 
+    return 0;
+}
