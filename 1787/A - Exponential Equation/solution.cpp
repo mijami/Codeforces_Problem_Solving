603655ff31faa1c9b33729474f7899ba0ffc3f5c
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:25:12 +0600

    Add Exponential Equation [A] from Codeforces

---
+++ 1787/A - Exponential Equation/solution.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define ll long long int
+#define pb push_back
+ 
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        if(n%2==0)
+        {
+            cout << n/2 << " " << 1 << "
+";
+        }
+        else
+        {
+            cout << "-1
+";
+        }
+ 
+    }
+    return 0;
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
