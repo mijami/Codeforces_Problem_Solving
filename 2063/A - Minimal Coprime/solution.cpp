a93d69d9d45e077c4448c7cdc5f74bb36779b5ee
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:52:25 +0600

    Add Minimal Coprime [A] from Codeforces

---
+++ 2063/A - Minimal Coprime/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define ll long long int
+#define ull unsigned long long int
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+#define newline cout << "
+";
+ 
+using namespace std;
+ 
+ 
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t=1;
+    cin >> t;
+    while(t--)
+    {
+        ll n,k;
+        cin >> n >> k;
+        
+        if(n==k && n==1) cout << "1
+";
+        else cout << k-n << "
+";
+ 
+    }
+    return 0;
+}
