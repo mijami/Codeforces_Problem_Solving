76bce9ea0a8cdb35a2142833abff7defcf84b5cf
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:05:53 +0600

    Add Toy Army [A] from Codeforces

---
+++ 84/A - Toy Army/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define checkmate return 0;
+#define pb push_back
+#define ll long long int
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int a,b;
+    cin >> a;
+ 
+    ll s=a/2+a;
+ 
+    cout << s << "
+";
+    checkmate
+}
