22d3c0b48df7eb104c4434de6ea67133f29b9045
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:36:47 +0600

    Add Sum in Binary Tree [C] from Codeforces

---
+++ 1843/C - Sum in Binary Tree/solution.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define ll long long int
+using namespace std;
+ 
+/*
+vector<pair<int,int>> vec[1000005];
+vector<int> dp;
+map<pair<int,int>,int> rec;
+ 
+ 
+void DFS(int current, int parent)
+{
+    for(auto i:vec[current])
+    {
+        int to= i.first;
+        int id= i.second;
+        if(to==parent)
+        {
+            continue;
+        }
+        rec[{current,to}]=id;
+        rec[{to,current}]=id;
+        if(dp[current]!=INT_MAX)
+        {
+            if(current==1)
+            {
+                dp[to]=1;
+            }
+            else
+            {
+                if(id>rec[{current,parent}])
+                {
+                    dp[to]=dp[current];
+                }
+                else{
+                    dp[to]=dp[current]+1;
+                }
+            }
+        }
+        DFS(to,current);
+    }
+}
+ 
+*/
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+ 
+        ll n ;
+        cin >> n ;
+ 
+ 
+        ll sum=0;
+        while(n>=1)
+        {
+            sum+=n;
+            n=n/2;
+        }
+        cout << sum << "
+";
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    }
+ 
+ 
+ 
+ 
+ 
+    return 0;
+}
+ 
