61c234720d67416162381aee0d718c301e814b18
Muminul Islam <66788357+mijami@users.noreply.github.com>
2026-09-18 03:35:46 +0600

    Add Binary Cafe [B] from Codeforces

---
+++ 1840/B - Binary Cafe/solution.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define ll long long int
+using namespace std;
+ 
+/*
+vector<pair<int,int>> vec[1000005];
+vector<int> dp;
+map<pair<int,int>,int> rec;
+ 
+ 
+void DFS(int current, int parent)
+{
+    for(auto i:vec[current])
+    {
+        int to= i.first;
+        int id= i.second;
+        if(to==parent)
+        {
+            continue;
+        }
+        rec[{current,to}]=id;
+        rec[{to,current}]=id;
+        if(dp[current]!=INT_MAX)
+        {
+            if(current==1)
+            {
+                dp[to]=1;
+            }
+            else
+            {
+                if(id>rec[{current,parent}])
+                {
+                    dp[to]=dp[current];
+                }
+                else{
+                    dp[to]=dp[current]+1;
+                }
+            }
+        }
+        DFS(to,current);
+    }
+}
+ 
+*/
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        int n,p;
+        cin >> n >> p;
+        p=min(p,30);
+        int x= pow(2,p);
+        x=min(x,n+1);
+        cout << x << "
+";
+ 
+    }
+ 
+    return 0;
+}
+ 
